9393a0d5313a35996cfebe209d405aa02c9ad208
Khalil Bessaad <bessaadkhalil@gmail.com>
2022-12-30 16:35:24 +0100

    small change

--- RMI/Server/src/Reverse.java
+++ RMI/Server/src/Reverse.java
@@ -9,11 +9,18 @@ public class Reverse extends UnicastRemoteObject implements ReverseInterface {
 
     @Override
     public String reverseString(String chaine) throws RemoteException {
-        int longeur = chaine.length();
-        StringBuffer tmp = new StringBuffer(longeur);
-        for (int i = longeur; i > 0; i--) {
-            tmp.append(chaine.substring(i - 1, i));
-        }
-        return tmp.toString();
+        // int longeur = chaine.length();
+        // StringBuffer tmp = new StringBuffer(longeur);
+        // for (int i = longeur; i > 0; i--) {
+        // tmp.append(chaine.substring(i - 1, i));
+        // }
+        // return tmp.toString();
+
+        // reverse string
+        return new StringBuffer(chaine).reverse().toString();
+
+        // q: why use StringBuffer? a: because it is mutable otherwise it would be
+        // immutable and we would have to create a new string
+
     }
 }
